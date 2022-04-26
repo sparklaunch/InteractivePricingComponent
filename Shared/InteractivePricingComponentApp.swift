dd6562050dbcf20decf4642ea168e872532a948a
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-26 09:25:00 +0900

    Initial Commit

---
+++ Shared/InteractivePricingComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractivePricingComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct InteractivePricingComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
